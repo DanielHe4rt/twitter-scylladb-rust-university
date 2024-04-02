97a41d025c564dfd201725b3573a3a4363179f2e
danielhe4rt <hey@danielheart.dev>
2024-04-02 14:41:27 +0100

    feat: step2 with prepared-statements

--- step2/src/repositories/timeline_service.rs
+++ step2/src/repositories/timeline_service.rs
@@ -10,7 +10,7 @@ use crate::models::timeline::Timeline;
 use crate::models::tweet::Tweet;
 
 pub trait TimelineServiceTrait {
-    async fn insert_to_timeline(&self, username: &str, tweet: &Tweet) -> Result<Timeline, QueryError>;
+    async fn insert_to_timeline(&self, username: &str, tweet: &Tweet) -> Result<(), QueryError>;
 
     async fn get_timeline_by_username(&self, username: &str) -> Result<(), QueryError>;
 }
@@ -20,7 +20,7 @@ pub struct TimelineService {
 }
 
 impl TimelineServiceTrait for TimelineService {
-    async fn insert_to_timeline(&self, username: &str, tweet: &Tweet) -> Result<Timeline, QueryError> {
+    async fn insert_to_timeline(&self, username: &str, tweet: &Tweet) -> Result<(), QueryError> {
         let random_liked = rand::random::<bool>();
         let random_bookmarked = rand::random::<bool>();
         let random_retweeted = rand::random::<bool>();
@@ -33,11 +33,14 @@ impl TimelineServiceTrait for TimelineService {
             liked: random_liked,
             bookmarked: random_bookmarked,
             retweeted: random_retweeted,
-            created_at: Uuid::now_v1(&[1,2,3,4,5,6]),
+            created_at: Uuid::now_v1(&[1, 2, 3, 4, 5, 6]),
         };
 
-        let timeline_insert_query = format!(
-            "INSERT INTO mykeyspace.timeline (username, tweet_id, author, text, liked, bookmarked, retweeted, created_at) VALUES ('{}', {}, '{}', '{}', {}, {}, {}, {})",
+        let timeline_insert_query = self.connection.prepare(
+            "INSERT INTO mykeyspace.timeline (username, tweet_id, author, text, liked, bookmarked, retweeted, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+        ).await?;
+
+        let payload = (
             timeline.username,
             timeline.tweet_id,
             timeline.author,
@@ -47,24 +50,23 @@ impl TimelineServiceTrait for TimelineService {
             timeline.retweeted,
             CqlTimeuuid::from_str(tweet.created_at.to_string().as_str()).unwrap()
         );
-        println!("{}", timeline_insert_query);
 
-        let session = self.connection.query(timeline_insert_query, &[]).await;
+        println!("{:?}", timeline_insert_query);
+
+        let session = self.connection.execute(&timeline_insert_query, payload).await;
 
         match session {
-            Ok(_) => Ok(timeline),
+            Ok(_) => Ok(()),
             Err(e) => Err(e),
         }
     }
 
     async fn get_timeline_by_username(&self, username: &str) -> Result<(), QueryError> {
+        let timeline_select_query = self.connection.prepare(
+            "SELECT username, tweet_id, author, text, liked, bookmarked, retweeted, created_at FROM mykeyspace.timeline WHERE username = ?",
+        ).await?;
 
-        let timeline_select_query = format!(
-            "SELECT username, tweet_id, author, text, liked, bookmarked, retweeted, created_at FROM mykeyspace.timeline WHERE username = '{}'",
-            username
-        );
-
-        let timeline = self.connection.query(timeline_select_query, &[]).await;
+        let timeline = self.connection.execute(&timeline_select_query, (username, )).await;
 
         match timeline {
             Ok(_res) => Ok(()),
