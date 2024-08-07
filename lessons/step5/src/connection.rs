5907bc5b89ff9131c68d7637e97ac154ea1cf490
danielhe4rt <hey@danielheart.dev>
2024-08-07 19:50:07 -0300

    feat: lessons 5 fix

--- lessons/step5/src/connection.rs
+++ lessons/step5/src/connection.rs
@@ -4,18 +4,18 @@ use scylla::statement::Consistency;
 
 pub async fn setup_connection() -> Session {
 
-    let policies = DefaultPolicy::builder()
-        .prefer_datacenter("DC1".to_string())
-        .build();
-
-    let execution_profile = ExecutionProfile::builder()
-        // .consistency(Consistency::LocalQuorum)
-        .load_balancing_policy(policies)
-        .build();
+    // let policies = DefaultPolicy::builder()
+    //     .prefer_datacenter("DC1".to_string())
+    //     .build();
+    //
+    // let execution_profile = ExecutionProfile::builder()
+    //     // .consistency(Consistency::LocalQuorum)
+    //     .load_balancing_policy(policies)
+    //     .build();
 
     let session = SessionBuilder::new()
         .known_nodes(vec!["localhost:9042", "localhost:9040", "localhost:9041"])
-        .default_execution_profile_handle(execution_profile.into_handle())
+        // .default_execution_profile_handle(execution_profile.into_handle())
         .build()
         .await
         .unwrap();
